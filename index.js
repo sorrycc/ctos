54168187d3e16d12f4c30ac64fd2c8e345aeaead
chencheng <sorrycc@gmail.com>
2014-05-10 21:42:53 +0800

    support repo transform, including all tags

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
 var util = require('util');
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
 var download = require('./lib/download');
 var transform = require('./lib/transform');
 var publish = require('./lib/publish');
 var log = require('./lib/util/log');
 
 module.exports = function(pkg, opt, cb) {
-  log.title('transform', pkg);
-
   if (typeof opt === 'function') {
     cb = opt, opt = null;
   }
+
+  var hasVersion = pkg.indexOf('@') > -1;
+  if (hasVersion) {
+    transformByVersion(pkg, opt, cb);
+    return;
+  }
+
+  getRelease(pkg, function(err, tags) {
+    async.eachSeries(tags, function(tag, next) {
+      var _pkg = pkg + '@' + tag;
+      transformByVersion(_pkg, opt, next);
+    }, cb);
+  });
+};
+
+function transformByVersion(pkg, opt, cb) {
+  log.title('transform', pkg);
   
   opt = opt || {};
   pkg = parse(pkg);
@@ -27,17 +46,48 @@ module.exports = function(pkg, opt, cb) {
   download(url, tmpDir, {extract:true}, function(err) {
     if (err) throw new Error(err);
 
-    // transform component package to spm@3x
     var dir = path.join(tmpDir, pkg.folder);
     log.info('download', 'end');
     log.info('dir', dir);
-    transform(dir); 
+
+    // valid
+    if (!fs.existsSync(path.join(dir, 'component.json'))) {
+      log.error('error', 'component.json not exist');
+      return cb(null);
+    }
+    var c = require(path.join(dir, 'component.json'));
+    if (!c.repo && !c.repository) {
+      log.error('error', 'repo or repository not found in component.json');
+      return cb(null);
+    }
+
+    // transform component package to spm@3x
+    transform(dir);
 
     // publish to spmjs.io
     publish(dir, function() {
       log.info('done');
       cb(null);
     });
+  });
+}
+
+function getRelease(repo, cb) {
+  var url = 'https://github.com/' + repo + '/releases';
+  log.info('fetch release', url);
+  request(url, function(err, res, body) {
+    if (err || res.statusCode != 200) {
+      log.error('error', err || res.statusCode);
+      return cb(err || 'res');
+    }
+    var $ = cheerio.load(body);
+    var tags = [];
+    $('div.tag-info span.tag-name').each(function() {
+      tags.push($(this).html().trim());
+    });
+    tags.reverse();
+    log.info('tags', tags.join(', '));
+    cb(null, tags);
   });
 }
 
@@ -52,4 +102,3 @@ function parse(pkg) {
   ret.folder = util.format('%s-%s', ret.name, ret.version);
   return ret;
 }
-
